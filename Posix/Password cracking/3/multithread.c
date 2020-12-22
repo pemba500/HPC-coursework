662f09acc04686251f78849fb2558f993065504b
Tsewang Namgyal Sherpa <pembahero10@gmail.com>
2020-12-22 12:48:20 +0545

    Password cracking using multiple thread

---
+++ Posix/Password cracking/3/multithread.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <crypt.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <pthread.h>
+#include <math.h>
+#include <unistd.h>
+
+
+
+/******************************************************************************
+  Demonstrates how to crack an encrypted password using a simple
+  "brute force" algorithm. Works on passwords that consist only of 2 uppercase
+  letters and a 2 digit integer. Your personalised data set is included in the
+  code. 
+
+  Compile with:
+    cc -o multithread multithread.c -lcrypt -pthread
+
+  If you want to analyse the results then use the redirection operator to send
+  output to a file that you can view using an editor or the less utility:
+
+    ./multithread > multithread.txt
+
+  Dr Kevan Buckley, University of Wolverhampton, 2020
+******************************************************************************/
+int num_password = 4;
+
+char *encrypted_pwd[] = {
+  "$6$AS$n8T7Ier9sC7nfcvidVU63kNIGRfv/Oqd/INE.B7F/DSp5wOr/Aoi5QTfUb1n1ELuPWmsnELl7xiTHRi6WbYG30",
+  "$6$AS$d8oVlHfMpszeU0.lOusjV44bj67Kjw5dze2oueelIQKVJyAR/BIdJ/8BGIyN36T1LRQrNIHvFcogOewjwRaiQ.",
+  "$6$AS$qdpclHBwMnumb31NQ5QPYJXspw3fkW6HZ2xi1uKjL8RKxTmTZekFhQBlLQi2HAcJ14q1vHtSlcojtWtFH0nAp0",
+  "$6$AS$zf1dT.NVcrDM0Bc/oKQKrVis.SkBJSt2EBi6E0MtbFNaAWwwmZh7i4uXTvR3DsMp.PCNeWUOEdk/11BP.1yXL."
+};
+
+/*
+ Required by lack of standard function in C.   
+*/
+
+void substr(char *dest, char *src, int start, int length){
+  memcpy(dest, src + start, length);
+  *(dest + length) = '\0';
+}
+
+/*
+ In this function, it can crack the kind of password given above. In result, 
+ when the password is found, # is kept at the start of the line. In this type of program 
+ comment out he printfs should not included because it is one of the most time consuming
+ operations.
+*/
+void PassCracks(){
+ int p;
+ pthread_t thread_1, thread_2;
+ 
+ void *kernel_function_1();
+ void *kernel_function_2();
+ 
+ for(p=0; p<num_password; p++)
+ { 
+  pthread_create(&thread_1, NULL, kernel_function_1, encrypted_pwd[p]);
+  pthread_create(&thread_2, NULL, kernel_function_2, encrypted_pwd[p]);
+
+  pthread_join(thread_1, NULL);
+  pthread_join(thread_2, NULL);
+ }
+}
+
+
+void *kernel_function_1(char *salt_and_encrypted){
+  int p, q, r;     // Counters loop
+  char salt[7];    // Need space for \0 while string used in hashing the password.
+  char plain[7];   // Checks the combination of letter
+  char *enc;       // Pointer to the encrypted password
+  int count = 0;   // The number of combinations explored so far
+
+  substr(salt, salt_and_encrypted, 0, 6);
+
+  for(p='A'; p<='M'; p++){
+    for(q='A'; q<='Z'; q++){
+      for(r=0; r<=99; r++){
+        sprintf(plain, "%c%c%02d", p, q, r); 
+        enc = (char *) crypt(plain, salt);
+        count++;
+        if(strcmp(salt_and_encrypted, enc) == 0){
+          printf("#%-8d%s %s\n", count, plain, enc);
+        }
+     
+      }
+    }
+  }
+  printf("%d solutions explored\n", count);
+}
+
+void *kernel_function_2(char *salt_and_encrypted){
+  int a, b, c;     // Counters loop
+  char salt[7];    // Need space for \0 while string used in hashing the password.
+  char plain[7];   // Checks the combination of letter
+  char *encrypt;       // Pointer to the encrypted password
+  int count = 0;   // The number of combinations explored so far
+
+  substr(salt, salt_and_encrypted, 0, 6);
+
+  for(a='N'; a<='Z'; a++){
+    for(b='A'; b<='Z'; b++){
+      for(c=0; c<=99; c++){
+        sprintf(plain, "%c%c%02d", a, b, c); 
+        encrypt = (char *) crypt(plain, salt);
+        count++;
+        if(strcmp(salt_and_encrypted, encrypt) == 0){
+          printf("#%-8d%s %s\n", count, plain, encrypt);
+        }
+   
+      }
+    }
+  }
+  printf("%d solutions explored\n", count);
+}
+
+
+/*Calculate the difference between two times and returns zero on
+  success and the time difference through an argument. It will 
+  be unsuccessful if the start time is after the end time. */
+
+int time_difference(struct timespec *start, 
+                    struct timespec *finish, 
+                    long long int *difference) {
+  long long int ds =  finish->tv_sec - start->tv_sec; 
+  long long int dn =  finish->tv_nsec - start->tv_nsec; 
+
+  if(dn < 0 ) {
+    ds--;
+    dn += 1000000000; 
+  } 
+  *difference = ds * 1000000000 + dn;
+  return !(*difference > 0);
+}
+
+int main(int argc, char *argv[])
+ {
+
+ struct timespec start, finish;   
+  long long int time_elapsed;
+ 
+  clock_gettime(CLOCK_MONOTONIC, &start);
+
+   PassCracks();
+
+  clock_gettime(CLOCK_MONOTONIC, &finish);
+   time_difference(&start, &finish, &time_elapsed);
+   printf("Time elapsed was %lldns or %0.9lfs\n", time_elapsed,
+                                         (time_elapsed/1.0e9)); 
+
+  return 0;
+  }
+
+
+
+
+ 
+  
+
+  
+
+
